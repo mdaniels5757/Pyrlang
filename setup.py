3508e750a6001387e4695a5e7304fee7f87af425
Michael Daniels <michael.daniels618738@tufts.edu>
2025-04-04 23:48:28 -0400

    +version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 PKGNAME = 'pyrlang'
-VERSION = '0.12-alpha'
+VERSION = '0.12-alpha1'
 DESCRIPTION = 'Erlang Node implemented in Python using asyncio'
 AUTHOR = 'Erlang Solutions Ltd and S2HC Sweden AB'
 AUTHOR_EMAIL = 'dmytro.lytovchenko@gmail.com,pyrlang@s2hc.com'
